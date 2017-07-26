0997e0ebbbdc89051446c32963ade9939b2bd5d8
damioune123 <damioune123>
2017-07-26 01:00:47 -0400

    fix subprocess bug cam encoding

--- cam/caisseAndFrigoCamTrigger.py
+++ cam/caisseAndFrigoCamTrigger.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
-import time
-import os
-import signal
-import subprocess
+import time, os, signal, subprocess
 port_caisse = 4
 port_frigo_1 = 14
 port_frigo_2 = 15
@@ -11,67 +8,47 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(port_caisse, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(port_frigo_1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(port_frigo_2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-caisseClosed=True
-frigoClosed=True
 def caisseOrFrigoOpened():
-    global caisseClosed
-    global frigoClosed
-    print('frigo/caisse closed v1: '+str(frigoClosed)+' / '+str(caisseClosed))
-    if not caisseClosed or not frigoClosed:
-        return True
     input_state_caisse =GPIO.input(port_caisse)
     input_state_frigo_1 =GPIO.input(port_frigo_1)
     input_state_frigo_2 =GPIO.input(port_frigo_2)
-    if not input_state_caisse:
-        caisseClosed = False
-    if not input_state_frigo_1 or  not input_state_frigo_2:
-        frigoClosed = False
-        print('frigo/caisse closed v2: '+str(frigoClosed)+' / '+str(caisseClosed))
+    if not input_state_caisse or (not input_state_frigo_1 and not input_state_frigo_2):
         return True
     else:
         return False
 
 def caisseAndFrigoClosed():
-    global caisseClosed
-    global frigoClosed
-    print('frigo/caisse closed v3: '+str(frigoClosed)+' / '+str(caisseClosed))
     input_state_caisse =GPIO.input(port_caisse)
     input_state_frigo_1 =GPIO.input(port_frigo_1)
     input_state_frigo_2 =GPIO.input(port_frigo_2)
-    if input_state_caisse:
-        caisseClosed = True
-    if input_state_frigo_1 and input_state_frigo_2:
-        frigoClosed = True
-    print('frigo/caisse closed v4: '+str(frigoClosed)+' / '+str(caisseClosed))
-    if frigoClosed and caisseClosed:
-        return True
+    if input_state_caisse and input_state_frigo_1 and input_state_frigo_2:
+         return True
     else:
         return False
-
-def subchild():
-    os.execl("encode_cam.sh", "")
 
 def child():
     newpid = os.fork()
     if newpid == 0:
-         subchild()
+         os.execl("stream_to_avi.sh", "")
     while True:
         time.sleep(0.5)
         if caisseAndFrigoClosed() == True:
-            os.kill(newpid, signal.SIGINT)
+            os.kill(newpid, signal.SIGTERM)
             os._exit(0)  
 
 def parent():
-    caisseClosed=True
-    frigoClosed=True
     while True:
-        time.sleep(0.5)
+        try:
+            time.sleep(0.5)
+        except KeyboardInterrupt:
+             print("Exiting frigo/caisse sensors daemon ...")
+             os._exit(0)
         if caisseOrFrigoOpened() == True:
             print('Caisse et/ou frigo  ouverts')
             newpid = os.fork()
             if newpid == 0:
                 child()
-            os.wait()
+            os.waitpid(-1, 0)
             print('Caisse et frigo fermes')
 
 parent()
